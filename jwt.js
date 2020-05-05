d8e4b0c53e97b8fbe98ffe331c59d1ef8e65b8a4
vencax <vencax@centrum.cz>
2020-05-05 15:10:33 +0200

    jwt login update

--- jwt.js
+++ jwt.js
@@ -14,8 +14,14 @@ app.post('/login', (req, res, next) => {
   const token = getToken(req)
   jwt.verify(token, process.env.SHARED_SECRET, (err, decoded) => {
     if (err) return next(err)
-    req.session.user = decoded
-    res.json(decoded)
+    if (decoded.user && decoded.user.local) {
+      req.session.user = decoded.user.local
+      req.session.user.id = decoded.user._id
+      req.session.user.groups = ['ooth']
+    } else {
+      req.session.user = decoded
+    }
+    res.json(req.session.user)
     next()
   })
 })
